bb456dc918225aa13357ceb1f5f7d4b383be803f
preethitrainingteksys <preethitrainingteksys@gmail.com>
2026-04-22 16:22:49 +0530

    implemented services for user service

---
+++ backend/services/user-service/src/main/java/com/flightapp/user_service/services/UserDetailsImplementation.java
@@ -0,0 +1,24 @@
+package com.flightapp.user_service.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.flightapp.user_service.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+public class UserDetailsImplementation implements UserDetailsService {
+	@Autowired
+	UserRepository userRepository;
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username)throws UsernameNotFoundException
+	{
+		User user=userRepository.findByUsername(username)
+				.orElseThrow(()->new UsernameNotFoundException("User Not Founf with username: "+username));
+		if(user.isBlocked())
+		{
+			throw new org.springframework.security.authentication.LockedException("User account is blocked!");
+		}
+		return UserDetailsImplementation.build(user);
+	}
+
+}
