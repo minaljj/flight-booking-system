73a6ac374bfeaa6be177a92e0f3b48d2ed94f2d2
Gunavaty <gunavatytraining.com>
2026-04-24 16:58:30 +0530

    removed redundant search-service

--- backend/services/search-service/routes/searchRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router() ;
-const searchController = require('../controller/searchController');
-
-router.get('/search', searchController.search);
-module.exports = router;
