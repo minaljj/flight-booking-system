704da028511ed64e67d06268510e0876436ed075
cressmerry <232644700+cressmerry@users.noreply.github.com>
2026-04-27 11:12:24 +0530

    Made a test suite to unify the tests

---
+++ backend/services/user-service/src/test/java/com/flightapp/user_service/UserServiceTestSuite.java
@@ -0,0 +1,13 @@
+package com.flightapp.user_service;
+
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+ 
+@Suite  
+@SuiteDisplayName("UserServiceTestSuite")
+@SelectPackages("com.flightapp.user_service")
+public class UserServiceTestSuite {
+   
+}
